e6d3ccbe4693f7f01b1d56dfa41b97812be102e6
Karima <188717305+Ka-di1@users.noreply.github.com>
2026-01-23 13:03:53 +0100

    Delete Post-assembly/Annotation/BAKTA.sh

--- Post-assembly/Annotation/BAKTA.sh
+++
@@ -1,7 +0,0 @@
-conda activate post-assembly 
-cd ~
-wget --continue https://zenodo.org/record/7025248/files/db.tar.gz  820   1586  sudo apt update
-
-ls -lh db.tar.gz
-mkdir -p bakta_db
-tar -xzf db.tar.gz -C bakta_db
